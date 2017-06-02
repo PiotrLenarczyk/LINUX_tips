8a39e9d1f6695c59163546c212d3d0842e87e5b0
PiotrLenarczykAnonim <piotr.lenarczyk@wat.edu.pl>
2017-06-02 11:12:44 +0200

    IPv6

--- 15_energyService/energyService.sh
+++ 15_energyService/energyService.sh
@@ -3,13 +3,14 @@ tresh="0.2"
 loadPC=(`cat /proc/loadavg`); echo "host loadvg: "${loadPC[2]}" (treshold=$tresh)"
 echo "=========================================================="
 echo "checking servers loads:"
-SERVERS=("localhost" "192.168.0.11")
+SERVERS=("localhost" "IPAddr")
 for SERVER in "${SERVERS[@]}"
 do
     loadPC=(`ssh lenarczp@$SERVER 'cat /proc/loadavg'`)
     echo "[ $SERVER ] loadavg: "${loadPC[2]}
     if (( `echo "${loadPC[2]} < $tresh" | bc -l` == 1 ))
     then
+#         shutdown now
         echo "[ $SERVER ] shutdown now"
     fi
     echo "----------------------------------------------------------"
