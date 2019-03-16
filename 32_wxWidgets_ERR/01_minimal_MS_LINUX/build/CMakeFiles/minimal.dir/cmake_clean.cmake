98deaa8e3672dc3da4c556741ade7e310e460802
PiotrLenarczyk <piotr.lenarczyk@wat.edu.pl>
2019-03-16 08:11:05 +0100

    update

---
+++ 32_wxWidgets_ERR/01_minimal_MS_LINUX/build/CMakeFiles/minimal.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/minimal.dir/minimal.cpp.o"
+  "minimal.pdb"
+  "minimal"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/minimal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
