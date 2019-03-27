46098be1a7491dbf9b0200750abb75afca644639
PiotrLenarczyk <piotr.lenarczyk@wat.edu.pl>
2019-03-27 20:31:48 +0100

    small update

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd CPlusplus_examples/ && git pull && cd ..
 cd CUDA_examples/ && git pull && cd ..
-cd LINUX_tips/ && git pull 
+cd LINUX_tips/ && git pull && cd ..
+cd scc/	&& git pull
 exit 0
