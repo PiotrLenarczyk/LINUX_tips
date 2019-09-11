431834aafc43c7a8319062fe9b19f23991b330c7
Piotr Lenarczyk <piotrl@elprotronic.com>
2019-09-11 13:24:06 +0200

    native compilers

--- 29_GCC_ARM/stm/defaults.c
+++ 29_GCC_ARM/stm/defaults.c
@@ -776,3 +776,151 @@ format:
 //======================================================================
 //======================================================================
 //======================================================================
+//=======================================================================================
+//definitions of memory handling functions:
+int   memcmp( void *ptr1, void *ptr2, unsigned int num );
+void* memset( void *ptr, int value, unsigned int num ) ;
+void* memcpy( void *destinationPtr, void *sourcePtr, unsigned int num );
+void* memmove( void *destinationPtr, void *sourcePtr, unsigned int num );
+void* memchr( void *ptr, int value, unsigned int num );
+//=======================================================================================
+//all variables are global
+#define MEMMOVE_BUFSIZE	0x10
+#define CHARACTER_MASK	0xFFFFFF00
+#define memPtr_i1	memPtr_iDst
+#define memPtr_i2	memPtr_iSrc
+#define memPtr_c1	memPtr_cDst
+#define memPtr_c2	memPtr_cSrc
+#define mem_i		mem_valueInt
+unsigned int 	*memPtr_iSrc, *memPtr_iDst,
+				mem_noInts, mem_valueInt;
+unsigned char 	*memPtr_cSrc, *memPtr_cDst, *memPtr_buf,
+				mem_buf[ MEMMOVE_BUFSIZE ];
+
+//=======================================================================================
+int memcmp( void *ptr1, void *ptr2, unsigned int num )
+{	memPtr_i1 = (unsigned int*)ptr1;
+	memPtr_i2 = (unsigned int*)ptr2;
+	mem_noInts = num >> 2; //divide by 4
+	while (mem_noInts > 0)
+	{	if (*memPtr_i1 != *memPtr_i2)
+		{	memPtr_c1 = (unsigned char *)memPtr_i1;
+			memPtr_c2 = (unsigned char *)memPtr_i2;
+			for ( mem_i = 0x0; mem_i < sizeof( int ); mem_i++ )
+			{	if ( *(memPtr_c1 + mem_i) != *(memPtr_c2 + mem_i) )
+					return ( (int)*(memPtr_c1 + mem_i) - (int)*(memPtr_c2 + mem_i) );
+			};
+		};
+		mem_noInts--;
+		memPtr_i1++;
+		memPtr_i2++;
+	};
+	mem_noInts = num >> 2; //divide by 4
+	num -= mem_noInts << 0x2; //reduce num of int number
+	if ( mem_noInts > 0x1 ) //if (mem_noInts > 1)
+	{	--memPtr_i1;
+		--memPtr_i2;
+	};
+	//byte comparision of unaligned rest
+	memPtr_c1 = (unsigned char *)memPtr_i1;
+	memPtr_c2 = (unsigned char *)memPtr_i2;
+	while (num > 0)
+	{	if (*memPtr_c1 != *memPtr_c2)
+			return (*memPtr_c1 - *memPtr_c2);
+		num--;
+		memPtr_c1++;
+		memPtr_c2++;
+	};
+	return 0;
+};
+
+//=======================================================================================
+void* memset( void *ptr, int value, unsigned int num )
+{	memPtr_i1 = (unsigned int*)ptr;
+	mem_noInts = num >> 0x2;
+	//====(value &= 0xFF) || (value &= 0xFFFFFFFF)====
+	if ( (value & CHARACTER_MASK) == 0x0 ) //set int size with character
+	{	mem_valueInt  = (unsigned int)value << 0x00;
+		mem_valueInt |= (unsigned int)value << 0x08;
+		mem_valueInt |= (unsigned int)value << 0x10;
+		mem_valueInt |= (unsigned int)value << 0x18;
+	};
+	while( mem_noInts-- )
+		*memPtr_i1++ = mem_valueInt;
+	mem_noInts = num >> 0x2;
+	num -= mem_noInts << 0x2;	//reduce num of ints size number
+	if ( mem_noInts > 0x1 ) 	//if (mem_noInts > 1)
+		memPtr_i1--;
+	//byte memory set with value of unaligned rest
+	memPtr_c1 = (unsigned char *)memPtr_i1;
+	while( num-- )
+		*memPtr_c1++ = (unsigned char)value;
+	return ptr;
+};
+
+//=======================================================================================
+void* memcpy( void *destinationPtr, void *sourcePtr, unsigned int num )
+{	memPtr_iDst = (unsigned int *)destinationPtr;
+	memPtr_iSrc = (unsigned int *)sourcePtr;
+	mem_noInts = num >> 0x2; //divide by 4
+	while( mem_noInts-- )
+		*memPtr_iDst++ = *memPtr_iSrc++;
+	mem_noInts = num >> 0x2;
+	num -= mem_noInts << 0x2;	//reduce num of ints size number
+	if ( mem_noInts > 0x1 ) 	//if (mem_noInts > 1)
+	{	memPtr_iDst--;
+		memPtr_iSrc--;
+	};
+	//byte memory move
+	memPtr_cSrc = (unsigned char *)memPtr_iSrc;
+	memPtr_cDst = (unsigned char *)memPtr_iDst;
+	while( num-- )
+		*memPtr_cDst++ = *memPtr_cSrc++;
+	return destinationPtr;
+};
+
+//=======================================================================================
+void* memmove( void *destinationPtr, void *sourcePtr, unsigned int num )
+{	if ( num > MEMMOVE_BUFSIZE )	//num<MEMMOVE_BUFSIZE
+		return (void*)-1;
+	memPtr_buf = mem_buf;
+	memPtr_cSrc = (unsigned char *)sourcePtr;
+	memPtr_iDst = (unsigned int *)destinationPtr;
+	memPtr_iSrc = (unsigned int *)sourcePtr;
+	mem_noInts = num;
+	//get data into memmove buffer
+	while( mem_noInts-- )
+		*memPtr_buf++ = *memPtr_cSrc++;
+	//copuy into dst from buffer via intPtr
+	memPtr_iSrc = (unsigned int *)mem_buf;
+	mem_noInts = num >> 0x2; //divide by 4
+	while( mem_noInts-- )
+		*memPtr_iDst++ = *memPtr_iSrc++;
+	mem_noInts = num >> 0x2;
+	num -= mem_noInts << 0x2;	//reduce num of ints size number
+	if ( (num >> 0x2) > 0x1 ) 	//if (mem_noInts > 1)
+	{	memPtr_iDst--;
+		memPtr_iSrc--;
+	};
+	//byte memory move
+	memPtr_cSrc = (unsigned char *)memPtr_iSrc;
+	memPtr_cDst = (unsigned char *)memPtr_iDst;
+	while( num-- )
+		*memPtr_cDst++ = *memPtr_cSrc++;
+	return destinationPtr;
+};
+
+//=======================================================================================
+void* memchr( void *ptr, int value, unsigned int num )
+{	memPtr_cSrc = (unsigned char *)ptr;
+	mem_noInts = num;
+	mem_i = 0x0;
+	while( (mem_noInts--) && (*memPtr_cSrc++ != (unsigned char)value) )
+		mem_i++;
+	if ( mem_i == num )
+		return 0x0;
+	return (void*)--memPtr_cSrc;
+};
+//======================================================================
+//======================================================================
+//======================================================================
--- 34_bareMetalCompilers/build_nativeCompiler.sh
+++ 34_bareMetalCompilers/build_nativeCompiler.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #http://www.iwasz.pl/electronics/toolchain-for-cortex-m4/
+#http://crosstool-ng.org/start
 sudo aptitude install -y bison flex gperf texinfo gawk libtool libtool-bin automake libncurses5-dev libexpat-dev help2man
 git clone https://github.com/crosstool-ng/crosstool-ng
 cd crosstool-ng/ && ./bootstrap
@@ -19,8 +20,14 @@ mkdir -p ../staging && cd ../staging/ && ../crosstool-ng/ct-ng menuconfig
 #	Target architecture: arm
 #	Default instruction set mode: thumb
 #================================================
-#	Architecture level: armv6-m				#CM0+
-#	Emit assembly for CPU: cortex-m0plus	#CM0+
+#			ALL TARGETS OPTIONS:
+#https://gcc.gnu.org/onlinedocs/gcc/Submodel-Options.html#Submodel-Options
+#	Architecture level: -march=XXXX
+#================================================
+#	Architecture level (-march): 			#CM0+
+#						armv6-m				#CM0+
+#	Emit assembly for CPU (-mtune): 		#CM0+
+#						cortex-m0plus		#CM0+
 #	Use specific FPU: "" ( none )			#CM0+
 #	Floating point: software - no FPU		#CM0+
 #================================================
@@ -28,6 +35,24 @@ mkdir -p ../staging && cd ../staging/ && ../crosstool-ng/ct-ng menuconfig
 #	Emit assembly for CPU: cortex-m4		#CM4F
 #	Use specific FPU:  fpv4-sp-d16			#CM4F
 #	Floating point: hardware ( FPU )		#CM4F
+#================================================
+#	####basic MIPS arch for learning purposes:
+#	Architecture level: mips1				#MIPS
+#	Floating point: software - no FPU		#MIPS
+#================================================
+#	Target architecture: avr				#AVR
+#	####default "avr" mcu cores access:		#AVR
+#	####avr25; avr3; avr31; avr34; avr35; 	#AVR
+#	####avr4; avr5; avr51; avr6; 			#AVR
+#	####avrxmega2/4/5/6/7; avrtiny; msp8	#AVR
+#   no avr-target specific configuration	#AVR
+#C-library:									#AVR
+#	C-library: avr-libc						#AVR
+#================================================
+#	Architecture level (-march): 			#x86-32
+#						blank: i386			#x86-32
+#						i586				#x86-32
+#	Bitness:	32bit						#x86-32
 #================================================
 #Toolchain options:
 #	Toolchain ID string: cm0plus
@@ -37,6 +62,7 @@ mkdir -p ../staging && cd ../staging/ && ../crosstool-ng/ct-ng menuconfig
 #Binary utilities
 #	Version of binutils: 2.31.1
 #C-library:
+#	C-library: newlib
 #	Version of newlib: 3.0.0
 #	Y: Disable the syscalls supplied with newlib
 #C compiler:
@@ -47,6 +73,8 @@ mkdir -p ../staging && cd ../staging/ && ../crosstool-ng/ct-ng menuconfig
 #	Y: GDB
 #		Version of gdb: 8.2
 #		N: Enable python scripting
+#Save config:
+#	".config"
 unset LD_LIBRARY_PATH 
 ../crosstool-ng/ct-ng build
 #secondary terminal:
